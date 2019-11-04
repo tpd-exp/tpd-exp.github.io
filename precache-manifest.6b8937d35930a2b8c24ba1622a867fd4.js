d9c0998e316592f276eda55405cdc10aae00db21
Skyle <skyle@gapp.nthu.edu.tw>
2019-11-04 15:41:08 +0800

    Updates

---
+++ precache-manifest.6b8937d35930a2b8c24ba1622a867fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ff80d6942882a314c3354258df61fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5405789dec4ad5a39c7b",
+    "url": "/static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "d73846c1ad071173f467",
+    "url": "/static/css/main.deb35e26.chunk.css"
+  },
+  {
+    "revision": "5405789dec4ad5a39c7b",
+    "url": "/static/js/2.8f4c3f3b.chunk.js"
+  },
+  {
+    "revision": "d73846c1ad071173f467",
+    "url": "/static/js/main.97fa23b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
